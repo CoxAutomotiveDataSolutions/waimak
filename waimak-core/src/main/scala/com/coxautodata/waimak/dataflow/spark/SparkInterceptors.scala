287c8f707ad5208489a90a61d704abf22cb17311
Vicky Avison <vicky.avison@coxauto.co.uk>
2018-07-03 12:24:51 +0100

    Sorting out interceptors

--- waimak-core/src/main/scala/com/coxautodata/waimak/dataflow/spark/SparkInterceptors.scala
+++ waimak-core/src/main/scala/com/coxautodata/waimak/dataflow/spark/SparkInterceptors.scala
@@ -14,13 +14,13 @@ object SparkInterceptors extends Logging {
 
   type CacheActionType = CachePostAction[Dataset[_], SparkFlowContext]
 
-  def addPostAction[T](sparkFlow: SparkDataFlow
+  def addPostAction(sparkFlow: SparkDataFlow
                     , outputLabel: String
-                    , postAction: PostAction[T, SparkFlowContext]): SparkDataFlow = {
+                    , postAction: PostAction[Any, SparkFlowContext]): SparkDataFlow = {
     val toIntercept: DataFlowAction[SparkFlowContext] = sparkFlow.getActionByOutputLabel(outputLabel)
     val interceptor = toIntercept match {
       // Interceptor already exists
-      case i: PostActionInterceptor[T, SparkFlowContext] => i.addPostAction(postAction)
+      case i@PostActionInterceptor(_, _) => i.addPostAction(postAction)
       // First interceptor
       case _ => PostActionInterceptor(toIntercept, Seq(postAction))
     }
@@ -30,10 +30,11 @@ object SparkInterceptors extends Logging {
   def addPostCacheAsParquet(sparkFlow: SparkDataFlow, outputLabel: String)
                            (dfFunc: Dataset[_] => Dataset[_])
                            (dfwFunc: DataFrameWriter[_] => DataFrameWriter[_]): SparkDataFlow = {
-    def post(data: Option[Dataset[_]], sfc: SparkFlowContext): Option[Dataset[_]] = {
+    def post(data: Option[Any], sfc: SparkFlowContext): Option[Dataset[_]] = {
       logInfo(s"About to cache the $outputLabel. Dataset is defined: ${data.isDefined}")
       val baseFolder = sparkFlow.tempFolder.getOrElse(throw new DataFlowException("Cannot cache, temporary folder was not specified"))
       data
+        .map(checkIfDataset(_, outputLabel, "addPostCacheAsParquet"))
         .map(dfFunc)
         .map { ds =>
           val path = new Path(new Path(baseFolder.toString), outputLabel).toString
@@ -57,9 +58,17 @@ object SparkInterceptors extends Logging {
     }
   }
 
-  def addPostTransform(sparkFlow: SparkDataFlow, outputLabel: String)(transform: (Dataset[_]) => Dataset[_]): SparkDataFlow = {
-    def post(data: Option[Dataset[_]], sfc: SparkFlowContext): Option[Dataset[_]] = data.map(transform)
+  def addPostTransform(sparkFlow: SparkDataFlow, outputLabel: String)(transform: Dataset[_] => Dataset[_]): SparkDataFlow = {
+    def post(data: Option[Any], sfc: SparkFlowContext): Option[Dataset[_]] =
+      data
+        .map(checkIfDataset(_, outputLabel, "addPostTransform"))
+        .map(transform)
 
     addPostAction(sparkFlow, outputLabel, TransformPostAction(post, outputLabel))
+  }
+
+  def checkIfDataset(value: Any, label: String, attemptedOperation: String): Dataset[_] = {
+    if (!value.isInstanceOf[Dataset[_]]) throw new DataFlowException(s"Can only call $attemptedOperation on a Dataset. Label $label is a ${value.getClass}")
+    else value.asInstanceOf[Dataset[_]]
   }
 }
