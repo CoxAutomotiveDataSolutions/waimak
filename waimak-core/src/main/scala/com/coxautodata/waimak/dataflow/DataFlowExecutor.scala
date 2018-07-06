6fe5ab3d57766c52d98686ea52a6ba663beaa719
Alexei Perelighin <alexei.perelighin@coxauto.co.uk>
2018-07-06 13:20:26 +0100

    Simplified DataFlowExecutor.loopExecution

--- waimak-core/src/main/scala/com/coxautodata/waimak/dataflow/DataFlowExecutor.scala
+++ waimak-core/src/main/scala/com/coxautodata/waimak/dataflow/DataFlowExecutor.scala
@@ -1,12 +1,14 @@
 package com.coxautodata.waimak.dataflow
 
+import com.coxautodata.waimak.log.Logging
+
 import scala.annotation.tailrec
 import scala.util.{Failure, Success, Try}
 
 /**
   * Created by Alexei Perelighin on 11/01/18.
   */
-trait DataFlowExecutor[C] {
+trait DataFlowExecutor[C] extends Logging {
 
   /**
     * Executes as many actions as possible with the given DAG, stops when no more actions can be executed.
@@ -18,7 +20,7 @@ trait DataFlowExecutor[C] {
   def execute(dataFlow: DataFlow[C]): (Seq[DataFlowAction[C]], DataFlow[C]) = {
     val preparedDataFlow = dataFlow.prepareForExecution()
 
-    val executionResults: Try[(Seq[DataFlowAction[C]], DataFlow[C])] = loopExecution(None, preparedDataFlow, initActionScheduler(), Seq.empty)
+    val executionResults: Try[(Seq[DataFlowAction[C]], DataFlow[C])] = loopExecution(preparedDataFlow, initActionScheduler(), Seq.empty)
 
     executionResults.get
   }
@@ -51,68 +53,67 @@ trait DataFlowExecutor[C] {
   }
 
   @tailrec
-  private def loopExecution(resultsToProcess: Option[ Seq[(DataFlowAction[C], Try[ActionResult])] ]
-                   , currentFlow: DataFlow[C]
-                    , actionScheduler: ActionScheduler[C]
-                    , successfulActions: Seq[DataFlowAction[C]]
-                   ): Try[(Seq[DataFlowAction[C]], DataFlow[C])] = {
-    //action scheduling and processing of the results are done in different loopExecution calls, this has simplified
-    //implementation of the state machine.
-    resultsToProcess match {
-      case Some(actionResults) => {
-        //process the results of the action execution
-        val (success, failed) = actionResults.partition(_._2.isSuccess)
-        val (newFlow, newSuccessfulActions) = success.foldLeft( (currentFlow, successfulActions) ) { (res, actionRes) =>
-          val action = actionRes._1
-          flowReporter.reportActionFinished(action, currentFlow.flowContext)
-          val nextFlow = res._1.executed(action, actionRes._2.get)
-          (nextFlow, res._2 :+ action)
-        }
-        if (failed.isEmpty) {
-          // continue with scheduling
-          loopExecution(None, newFlow, actionScheduler, newSuccessfulActions)
-        } else {
-          failed.foreach{ t =>
-//            logErrro(t)
-            // TODO: maybe add to flowReporter info about failed actions
-            println("DEBUG failed " + t._1.logLabel + " " + t._2.failed)
-          }
-          val failedAction = failed.head._1
-          //
-          Failure(new DataFlowException(s"Exception performing action: ${failedAction.logLabel}", failed.head._2.failed.get)) //:face_palm:
-        }
+  private def loopExecution(currentFlow: DataFlow[C]
+                            , actionScheduler: ActionScheduler[C]
+                            , successfulActions: Seq[DataFlowAction[C]]
+                           ): Try[(Seq[DataFlowAction[C]], DataFlow[C])] = {
+    val toSchedule = if (actionScheduler.canSchedule()) {
+      priorityStrategy(actionScheduler.dropRunning(currentFlow.nextRunnable())).headOption
+    } else None
+    toSchedule match {
+      case None if (!actionScheduler.hasRunningActions()) => {
+        logInfo(s"Flow exit successfulActions: ${successfulActions.mkString("[", "", "]")} remaining: ${currentFlow.actions.mkString("[", ",", "]")}")
+        Success((successfulActions, currentFlow))
       }
       case None => {
-        if (actionScheduler.canSchedule()) {
-          val toSchedule: Option[DataFlowAction[C]] = priorityStrategy(actionScheduler.dropRunning(currentFlow.nextRunnable())).headOption
-          toSchedule match {
-            case None if (!actionScheduler.hasRunningActions()) => {
-//              logInfo("Finished Flow" + successfulActions)
-              println(s"DEBUG Flow exit successfulActions: ${successfulActions.mkString("[","", "]")} remaining: ${currentFlow.actions.mkString("[", ",", "]")}")
-              Success((successfulActions, currentFlow))
-            }
-            case None => {
-              actionScheduler.waitToFinish() match {
-                case Success((newScheduler, actionResults)) => loopExecution(Some(actionResults), currentFlow, newScheduler, successfulActions)
-                case Failure(e) => Failure(e)
-              }
-            }
-            case Some(action) => {
-              //submit action for execution
-              flowReporter.reportActionStarted(action, currentFlow.flowContext)
-              val inputEntities: DataFlowEntities = {
-                currentFlow.inputs.filterLabels(action.inputLabels)
-              }
-              loopExecution(None, currentFlow, actionScheduler.submitAction(action, inputEntities, currentFlow.flowContext), successfulActions)
+        actionScheduler.waitToFinish() match {
+          case Success((newScheduler, actionResults)) => {
+            processActionResults(actionResults, currentFlow, successfulActions) match {
+              case Success((newFlow, newSuccessfulActions)) => loopExecution(newFlow, newScheduler, newSuccessfulActions)
+              case Failure(e) => Failure(e)
             }
           }
-        } else {
-          actionScheduler.waitToFinish() match {
-            case Success((newScheduler, actionResults)) => loopExecution(Some(actionResults), currentFlow, newScheduler, successfulActions)
-            case Failure(e) => Failure(e)
-          }
+          case Failure(e) => Failure(e)
         }
       }
+      case Some(action) => {
+        //submit action for execution
+        flowReporter.reportActionStarted(action, currentFlow.flowContext)
+        val inputEntities: DataFlowEntities = {
+          currentFlow.inputs.filterLabels(action.inputLabels)
+        }
+        loopExecution(currentFlow, actionScheduler.submitAction(action, inputEntities, currentFlow.flowContext), successfulActions)
+      }
+    }
+  }
+
+  /**
+    *
+    * @param actionResults
+    * @param currentFlow
+    * @param successfulActionsUntilNow
+    * @return                   Success((new state of the flow, appended list of successful actions)), Failure will be returned
+    *                           if at least one action in the actionResults has failed
+    */
+  private[dataflow] def processActionResults(actionResults: Seq[(DataFlowAction[C], Try[ActionResult])]
+                                             , currentFlow: DataFlow[C]
+                                             , successfulActionsUntilNow: Seq[DataFlowAction[C]]): Try[(DataFlow[C], Seq[DataFlowAction[C]])] = {
+    val (success, failed) = actionResults.partition(_._2.isSuccess)
+    val res = success.foldLeft( (currentFlow, successfulActionsUntilNow) ) { (res, actionRes) =>
+      val action = actionRes._1
+      flowReporter.reportActionFinished(action, currentFlow.flowContext)
+      val nextFlow = res._1.executed(action, actionRes._2.get)
+      (nextFlow, res._2 :+ action)
+    }
+    if (failed.isEmpty) {
+      Success(res)
+    } else {
+      failed.foreach{ t =>
+        // TODO: maybe add to flowReporter info about failed actions
+        logError("Failed Action " + t._1.logLabel + " " + t._2.failed)
+      }
+      val failedAction = failed.head._1
+      Failure(new DataFlowException(s"Exception performing action: ${failedAction.logLabel}", failed.head._2.failed.get)) //:face_palm:
     }
   }
 
