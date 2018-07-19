1981eedb43502fd69911dbe3f0f6212584b2dab8
dj <dj@localhost.localdomain>
2018-07-19 13:47:13 +0100

    fixed a tiny bug cause by the previous commit

--- waimak-core/src/main/scala/com/coxautodata/waimak/dataflow/spark/ui/WaimakEventListener.scala
+++ waimak-core/src/main/scala/com/coxautodata/waimak/dataflow/spark/ui/WaimakEventListener.scala
@@ -10,7 +10,7 @@ class WaimakEventListener extends SparkListener {
   val executions = new mutable.HashMap[String, WaimakExecutionEvent]()
 
   override def onOtherEvent(event: SparkListenerEvent): Unit = event match {
-    case e@WaimakExecutionEvent(id, _, _) => executions(id) = e
+    case e@WaimakExecutionEvent(id, _) => executions(id) = e
     case _ => Unit
   }
 }
