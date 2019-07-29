cab2c25008d128841542b70264d51597a263d203
Alex Bush <alex.bush@coxauto.co.uk>
2019-07-29 16:41:08 +0100

    Initial implementation

---
+++ waimak-configuration/src/test/scala/com/coxautodata/waimak/configuration/TestPropertyProviderTrait.scala
@@ -0,0 +1,41 @@
+package com.coxautodata.waimak.configuration
+
+import org.scalatest.{FunSpec, Matchers}
+
+import scala.concurrent.TimeoutException
+import scala.util.Try
+
+class TestPropertyProviderTrait extends FunSpec with Matchers {
+
+  describe("getWithTimeout") {
+
+    it("should retry and succeed the third time") {
+      TestPropertyProviderInstance(List(1000, 1000))
+        .getWithRetry("", 200, 3) should be(Some("no timeout"))
+    }
+
+    it("should retry and succeed with a shorter timeout") {
+      TestPropertyProviderInstance(List(1000, 0))
+        .getWithRetry("", 200, 3) should be(Some("after timeout"))
+    }
+
+    it("should throw an exception due to timeout") {
+      intercept[TimeoutException] {
+        TestPropertyProviderInstance(List(1000))
+          .getWithRetry("", 200, 0)
+      }
+    }
+
+  }
+
+}
+
+case class TestPropertyProviderInstance(failures: List[Long]) extends PropertyProvider {
+
+  private val internalList = failures.toBuffer
+
+  override def get(key: String): Option[String] = Try {
+    Thread.sleep(internalList.remove(0))
+    Some("after timeout")
+  }.getOrElse(Some("no timeout"))
+}
