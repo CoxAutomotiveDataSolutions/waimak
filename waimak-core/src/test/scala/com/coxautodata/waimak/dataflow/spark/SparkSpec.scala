0d0037e7bb59d7fce4c122360fffba64f908f418
James Fielder <james@fielder.dev>
2021-05-17 23:08:47 +0100

    An initial go at fixing test options for spark 3.1 storage problem

--- waimak-core/src/test/scala/com/coxautodata/waimak/dataflow/spark/SparkSpec.scala
+++ waimak-core/src/test/scala/com/coxautodata/waimak/dataflow/spark/SparkSpec.scala
@@ -10,8 +10,8 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 /**
-  * Created by Vicky Avison on 24/10/17.
-  */
+ * Created by Vicky Avison on 24/10/17.
+ */
 trait SparkSpec extends AnyFunSpec with Matchers with BeforeAndAfterEach {
 
   var sparkSession: SparkSession = _
@@ -19,12 +19,16 @@ trait SparkSpec extends AnyFunSpec with Matchers with BeforeAndAfterEach {
   val appName: String
 
   /**
+   * In spark 3  */
+  def parquetLegacyModeForSpark2To3(enabled: Boolean = false): String =
+    if (enabled) "LEGACY" else "CORRECTED"
+
+  /**
    * Allows configuring spark session options. Override this in your own tests to set any
    * config options you might wish to add. This default implementation also detects if you are
    * running the tests on windows, and sets the bindAddress option to make the tests work.
    *
    * @see com.coxautodata.waimak.metastore.TestHiveDBConnector for an example
-   *
    * @return a spark session
    */
   def builderOptions: SparkSession.Builder => SparkSession.Builder = { sparkSession =>
@@ -41,10 +45,31 @@ trait SparkSpec extends AnyFunSpec with Matchers with BeforeAndAfterEach {
       .config("spark.ui.enabled", "false")
 
     sparkSession = builderOptions(preBuilder).getOrCreate()
+
+    getSparkVersion(sparkSession) match {
+      case SparkVersion(2, _, _) => ()
+      case SparkVersion(3, _, _) => configureParquetReaderWriterOptionsForSpark3(sparkSession)
+      case _ => ()
+    }
   }
 
   override def afterEach(): Unit = {
     sparkSession.stop()
+  }
+
+  def getSparkVersion(spark: SparkSession): SparkVersion =
+    spark.version.split(".").toList.map(_.toInt) match {
+      case maj :: min :: p :: Nil => SparkVersion(maj, min, Some(p))
+      case maj :: min :: Nil => SparkVersion(maj, min, None)
+      case _@v => throw new IllegalStateException(s"The spark version of ${v.mkString} cannot be parsed")
+    }
+
+  def configureParquetReaderWriterOptionsForSpark3(sparkSession: SparkSession): Unit = {
+    val setting = parquetLegacyModeForSpark2To3()
+    sparkSession.conf.set("spark.sql.legacy.parquet.int96RebaseModeInWrite", setting)
+    sparkSession.conf.set("spark.sql.legacy.parquet.int96RebaseModeInRead", setting)
+    sparkSession.conf.set("spark.sql.legacy.parquet.datetimeRebaseModeInWrite", setting)
+    sparkSession.conf.set("spark.sql.legacy.parquet.datetimeRebaseModeInRead", setting)
   }
 }
 
@@ -68,3 +93,4 @@ trait SparkAndTmpDirSpec extends SparkSpec {
   }
 }
 
+case class SparkVersion(major: Int, minor: Int, point: Option[Int])
